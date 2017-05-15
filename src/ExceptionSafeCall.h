3b2b09b6762a3f51ec0f0ee2a3bb1142adcc4c6f
Casey Waldren <casey@nullspacevr.com>
2017-05-15 16:32:42 -0700

    Allow exception catching to be disabled
    Comment out #define CATCH_EXCEPTIONS
    
    This is good for testing to see if the plugin is crashing and silently covering it up.

--- src/ExceptionSafeCall.h
+++ src/ExceptionSafeCall.h
@@ -1,11 +1,17 @@
 #pragma once
 #include "NSLoader_Errors.h"
 
+#define CATCH_EXCEPTIONS
+
 template<typename T> NSVR_Result ExceptionGuard(T&& t) {
+#ifdef CATCH_EXCEPTIONS
 	try {
+#endif
 		return t();
+#ifdef CATCH_EXCEPTIONS
 	}
 	catch (const std::exception&) {
 		return NSVR_Error_Unknown;
 	}
+#endif
 }
