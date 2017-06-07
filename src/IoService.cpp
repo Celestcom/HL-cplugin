54acb43bbdbf91dfb5f874d3b96545fedba59f45
Casey Waldren <casey@nullspacevr.com>
2017-06-07 11:22:56 -0700

    Remove very very old cruft from IoService
    Most of it was necessary to reset the service from a different thread, when we communicated with serial ports using the plugin. That functionality is now in the driver.

--- src/IoService.cpp
+++ src/IoService.cpp
@@ -11,10 +11,7 @@ IoService::IoService():
 	_io(), 
 	_work(), 
 	_running(false), 
-	_readyToResumeIO(2),
-	_shouldQuit{false}, 
-	_wantsReset{false}, 
-	_isReset{false}
+	_shouldQuit{false}
 {
 	start();
 }
@@ -24,60 +21,20 @@ void IoService::start() {
 		return;
 	}
 	_running = true;
-	_adapterResetLoop = std::thread([&]() {
-
-		for (;;) {
-			try {
-				//auto& log = Locator::Logger();
-				boost::unique_lock<boost::mutex> lock(_needToCheckMut); //unlocked at this point
-				//log.Log("IoService-r", "Waiting on the conditions");
-				//this thread will wakeup when either _shouldQuit or _wantsReset returns true, and aquire the lock
-				_needWakeup.wait(lock, [&] { return _shouldQuit.load() || _wantsReset.load();  });
-				if (!_shouldQuit.load()) {
-					//	log.Log("IoService-r", "Was awoken to do work");
-						//Okay, we have not quit, so it must be that we want a reset
-						//First, reset  _wantsReset to it's default state: false
-					_wantsReset.store(false);
-					//Then, we unlock the mutex so other threads can modify any of the variables
-					_io.stop();
-					//	log.Log("IoService-r", "Now waiting on the reset condition..");
-						//we will wakeup again once isReset becomes true
-					_doneResettingIO.wait(lock, [&] {return _isReset.load(); });
-					//	log.Log("IoService-r", "Okay, going to call the callback");
-					_isReset.store(false); //set back to default state
-					lock.unlock();
-					_io.post(_resetIOCallback);
-				}
-				else {
-					//we want to quit, so break the loop
-					break;
-				}
-			}
-			catch (boost::system::system_error&) {
-			//	std::cout << "BIG ERROR\n";
-			}
-		}
-
-	});
-
+	
 	_ioLoop = std::thread([&]() {
-		//Keep running as long as we haven't signaled to quit, but this is only checked after .run returns.
-		//.run will block until it is stopped
-		//auto& log = Locator::Logger();
+		
 		while (!_shouldQuit.load()) {
 			try {
 				_work = std::make_unique<boost::asio::io_service::work>(_io);
 
-				//log.Log("IoService-i", "Going to run");
 				_io.run(); //wait here for a while
-				//log.Log("IoService-i", "Going to reset and notify");
 				_io.reset(); //someone stopped us? Reset
 
-				_isReset.store(true);
-				_doneResettingIO.notify_one();
 			}
 			catch (boost::system::system_error&) {
 				//The Io service couldn't reset?!
+				//todo: log this 
 			}
 
 		}
@@ -87,20 +44,14 @@ void IoService::start() {
 }
 void IoService::Shutdown()
 {
-	{
-		boost::lock_guard<boost::mutex> lock(_needToCheckMut);
-		_shouldQuit.store(true);
-	}
-	_needWakeup.notify_one();
+	
+	_shouldQuit.store(true);
 	_io.stop();
 
 	if (_ioLoop.joinable()) {
 		_ioLoop.join();
 	}
 
-	if (_adapterResetLoop.joinable()) {
-		_adapterResetLoop.join();
-	}
 }
 
 IoService::~IoService()
@@ -111,19 +62,5 @@ IoService::~IoService()
 boost::asio::io_service& IoService::GetIOService()
 {
 	return _io;
-}
-
-void IoService::RestartIOService(std::function<void()> ioResetCallback)
-{
-//	Locator::Logger().Log ("IoService", "Someone requested a restart of IO service");
-	_resetIOCallback = ioResetCallback;
-
-	{
-		boost::lock_guard<boost::mutex> lock(_needToCheckMut);
-		_wantsReset.store(true);
-	}
-	_needWakeup.notify_one();
-	//Locator::Logger().Log("IoService", "I notified it");
-
 }
 
