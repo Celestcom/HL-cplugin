8f6fa29e82ec00b2900f9b5116604a61f19c4d11
Casey Waldren <caseywaldren@gmail.com>
2017-12-07 14:44:30 -0800

    Cleanup IOService

--- src/Plugin/IoService.cpp
+++ src/Plugin/IoService.cpp
@@ -1,64 +1,46 @@
 #include "StdAfx.h"
 #include "IoService.h"
-#include <iostream>
 
 
-//To those who would venture into this code:
-//It is very old. With a good testing suite, we may be able to replace it with something less complex
-//now that most of the serial issues are moved into the driver. 
-
-IoService::IoService():
-	_io(), 
-	_work(), 
-	_running(false), 
-	_shouldQuit{false}
+IoService::IoService()
+	: m_running{false}
+	, m_ioLoop{}
+	, m_io{}
+	, m_work{}
+	, m_shouldQuit{false}
 {
 	start();
 }
 
 void IoService::start() {
-	if (_running) {
-		return;
-	}
-	_running = true;
-	
-	_ioLoop = std::thread([&]() {
-		
-		while (!_shouldQuit.load()) {
+	m_running = true;
+	m_ioLoop = std::thread([&]() {
+		while (!m_shouldQuit.load()) {
 			try {
-				_work = std::make_unique<boost::asio::io_service::work>(_io);
-
-				_io.run(); //wait here for a while
-				_io.reset(); //someone stopped us? Reset
-
+				m_work = std::make_unique<boost::asio::io_service::work>(m_io);
+				m_io.run(); //wait here for a while
+				m_io.reset(); //someone stopped us? Reset
 			}
 			catch (boost::system::system_error&) {
-				//The Io service couldn't reset?!
+				//The io_service couldn't reset?
 				//todo: log this 
 			}
-
 		}
 	});
-	
-	
 }
+
 void IoService::Shutdown()
 {
-	
-	_shouldQuit.store(true);
-	_work.reset();
-
-	_io.stop();
-
-	if (_ioLoop.joinable()) {
-		_ioLoop.join();
+	m_shouldQuit.store(true);
+	m_work.reset();
+	m_io.stop();
+	if (m_ioLoop.joinable()) {
+		m_ioLoop.join();
 	}
-
 }
-
 
 boost::asio::io_service& IoService::GetIOService()
 {
-	return _io;
+	return m_io;
 }
 
--- src/Plugin/IoService.h
+++ src/Plugin/IoService.h
@@ -7,26 +7,18 @@
 class IoService
 {
 public:
-	//Constructs and Start's a new IoService
 	IoService();
-
-	//Return the underlying io_service object
 	boost::asio::io_service& GetIOService();
-
-
-	//Tell it to shutdown
 	void Shutdown();
-
 private:
+	bool m_running;
+	std::thread m_ioLoop;
+	boost::asio::io_service m_io;
+	std::unique_ptr<boost::asio::io_service::work> m_work;
+	std::atomic_bool m_shouldQuit;
 
 	void start();
-	bool _running;
-	bool _dataReady;
 
-	std::thread _ioLoop;
-	boost::asio::io_service _io;
-	std::unique_ptr<boost::asio::io_service::work> _work;
-	std::atomic_bool _shouldQuit;
 
 };
 
